0e9741c0326ec96e0752cdf4e2e100fae79b00d5
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2022-11-21 22:26:41 +0200

    Add files via upload

---
+++ Spele/README.md
@@ -0,0 +1,20 @@
+### Scenarijs
+
+1.)Speletajājs tiek sveicināts Sākuma Izvēlnē (Menu) palaižot aplikāciju.
+
+### Prasības
+
+ □ Spēles cikls
+ □ Dekstop Programma
+ □ Cilvēciņš ar kuru var kustēties
+ □ Darbības ar apkārtni
+ □ Inventory sistema
+ □ Punktu sistēma
+ □ Resursi
+ □ Karte random ģenerēšana ar opciju cik lielu pasauli spēlētājs vēlas
+ □ Dažādi objekti ar random ģenerēšanos ar opciju cik daudz tie būs
+ □  Objekti ar ko var kustēties
+ □ Savācamie objekti un savākšanas sistēma
+ □ Highsore tabula ar laiku
+ □ Menu
+ □ Ingame menu
---
+++ Spele/src/Menu.java
@@ -0,0 +1,57 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+public class Menu extends JPanel implements Runnable {//extends Papildina JPanel bibliotēku
+    //implement Runnable ļauj impelementot interfaces jeb klasi ar abstraktam metodem
+
+
+
+
+    //Ekrana iestatijumi
+    final int originalTitleSize = 16; //1 laukuma lielums 16x16 pixel
+    
+    //Taka musdienas 1920 x 1080 pixel ekrani tad vajag scalot
+    final int scale = 3; 
+
+    final int tileSize = originalTitleSize * scale;//16 reizin ar 3
+    
+    final int maxScreenRow = 12;//Laukuma Garums
+    final int screenHeight = tileSize * maxScreenRow;//576 pixeli
+
+    final int maxScreenCol = 16;//Laukuma Platums
+    final int screenWidth = tileSize * maxScreenCol;//768 pixeli
+
+    Thread gameThread;//Atļauj notikt vairākām darbībam vienalaikus
+
+    public Menu(){
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        getBackground();
+        this.setBackground(Color.green);
+        this.setDoubleBuffered(true);//viss zīmējums no šī komponenta tiks veikts offscreen krāsojumā, better games rendering performance 
+        
+
+
+    }
+
+    public void startGameThread(){
+
+        gameThread = new Thread(this);//This palaiz Menu šaja konstruktora
+        gameThread.start();//Palaidis run()
+
+    }
+
+
+
+
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+}
+
+
